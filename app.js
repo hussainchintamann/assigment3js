6399f79be598d86ebe5131c8592a2d0217e4009e
hussainchintamann <hussaindr369@gmail.com>
2024-02-05 21:30:44 +0500

    assignment 3 js done

---
+++ app.js
@@ -0,0 +1,186 @@
+
+// Question no 1
+// var city = prompt()
+// var value = city.toLowerCase();
+// if (value === "karachi")
+// {
+//     alert ("Welcome to city of lights”");
+// }
+
+// Question no 2
+
+// var gender = prompt ("Enter your gender");
+// var value = gender.toLowerCase();
+// if (value === "male")
+// {
+//     alert ("Good Morning sir")
+// }
+
+// if (value === "female")
+// {
+//     alert ("Good Morning ma'am ")
+// }
+
+// Question no 3
+
+// var car = +prompt()
+// if (car <= 0.25)
+// {
+//     alert ("“Please refill the fuel in your car” ")
+// }
+
+// Question no 4
+// part a
+//  var a = 4
+//  if (++a === 5)
+//  {
+//     alert("given condition for variable a is true"); 
+
+//  }
+// part b
+
+//  var b = 82; 
+// if (b++ === 83){ 
+// alert("given condition for variable b is false"); 
+// } 
+
+// part c
+
+//  var c = 12; 
+// if (c++ === 13){ 
+// alert("condition 1 is false"); 
+// } 
+// if (c === 13){ 
+// alert("condition 2 is true"); 
+// } 
+// if (++c < 14){ 
+// alert("condition 3 is false");
+// }
+
+// if(c === 14){ 
+//     alert("condition 4 is true"); 
+//     } 
+ 
+
+// part d 
+
+// var materialCost = 20000;
+// var labourCost = 2000;
+// var totalCost = materialCost + labourCost;
+// if (totalCost===labourCost + materialCost)
+// {
+//     alert("the cost is equal")
+// }
+
+// part e 
+
+// if (true)
+// {
+//     alert("True")
+// }
+// if (false)
+// {
+//     alert("False")
+// }
+
+// part f
+
+// if ("car" < "cat")
+// {
+//     alert("cat is smaller than car")
+// }
+
+// Question no 5
+
+// var game = +prompt("playing game");
+// if(game === 5)
+// {
+// alert ("you are winner")
+// }
+// else {
+//     alert("you are loser")
+// }
+
+// Question no 6
+
+// var divisible =+prompt("Enter number")
+// if (divisible % 3 === 0)
+// {
+//     alert ("The number" + " " + divisible +  " " + "is divisble by 3")
+
+// }
+// else{
+//     alert("The number " +  " "  + divisible +  " " + "not divisible by 3")
+// }
+
+// Question no 7
+
+// var number = +prompt("Enter Number")
+// if (number % 2 === 0)
+// {
+//     alert (number + " " + "is even")
+// }
+// else {
+//     alert (number + " " +  "is odd")
+// }
+
+// question 8
+
+// var tempreture = +prompt("Enter a tempreture number");
+// if (tempreture > 40 )
+// {
+//     alert ("It is too hot outside.")
+// }
+
+// else if (tempreture > 30 )
+// {
+//     alert("The Weather today is Normal")
+// }
+// else if (tempreture > 20 )
+// {
+//     alert("Today’s Weather is cool")
+// }
+
+// else if (tempreture >= 10)
+// {
+//     alert("OMG! Today’s weather is so Cool")
+// }
+
+// question 9
+
+// var firstValue = +prompt("Enter First Value");
+// var SecondValue = +prompt("Enter Second Value");
+// var operator = prompt ("Enter the operation (+.-.*./.%) ");
+
+
+// if (operator==="+")
+// {
+//     result = firstValue + SecondValue 
+// }
+
+
+// else if (operator === "-")
+// {
+//    result = firstValue - SecondValue
+// }
+
+// else if (operator ==="*")
+// {
+//     result= firstValue * SecondValue
+// }
+// else if (operator === "/")
+// {
+//     result = firstValue / SecondValue
+// }
+// else if (operator === "%")
+// {
+//    result = firstValue % SecondValue
+// }
+
+// else{
+//     alert("invalid operator")
+// }
+
+// alert( result );
+
+
